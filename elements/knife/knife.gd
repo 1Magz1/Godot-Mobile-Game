e6fc6829134404a547e1c1dac3de7a02d2d4a406
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-12-25 21:13:02 +0700

    main
    add_knife_to_target

--- elements/knife/knife.gd
+++ elements/knife/knife.gd
@@ -6,7 +6,23 @@ var is_flying := false
 
 func _physics_process(delta: float):
 	if is_flying:
-		move_and_collide(Vector2.UP * SPEED * delta)
+		var collision = move_and_collide(Vector2.UP * SPEED * delta)
+		handle_collision(collision)
 
 func throw():
 	is_flying = true
+
+func handle_collision(collision: KinematicCollision2D):
+	if collision == null: return
+	
+	var collider := collision.get_collider()
+
+	if collider is Target:
+		add_knife_to_target(collider)
+		is_flying = false
+
+func add_knife_to_target(target: Target):
+	get_parent().remove_child(self)
+	global_position = target.KNIFE_POSITION
+	rotation = 0
+	target.add_object_with_pivot(self, -target.rotation)
--- elements/targets/target/target.gd
+++ elements/targets/target/target.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
+class_name Target
 
-var speed := PI
+const SPEED := PI
+const KNIFE_POSITION := Vector2(0, 180)
+
+@onready var item_container := $ItemsContainer
 
 func _physics_process(delta: float):
-	rotation += speed * delta
+	rotation += SPEED * delta
+
+func add_object_with_pivot(object: Node2D, object_rotation: float):
+	var pivot := Node2D.new()
+	pivot.rotation = object_rotation
+	pivot.add_child(object)
+	item_container.add_child(pivot)
